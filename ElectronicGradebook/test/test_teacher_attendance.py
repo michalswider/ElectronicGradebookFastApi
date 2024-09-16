6bfdc634e9997c09fd145ab771de21c3afd0db89
Michał Świderski <michalswiderski41@gmail.com>
2024-09-16 11:11:05 +0200

    Added test_get_attendance_for_student_in_subject in pytest

--- ElectronicGradebook/test/test_teacher_attendance.py
+++ ElectronicGradebook/test/test_teacher_attendance.py
@@ -102,3 +102,9 @@ def test_get_attendance_for_class_on_date_attendance_not_found(test_student_with
     response = client.get('teacher/attendance/class/1/date?date=2024-09-04')
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'No attendance records found for class with id: 1 on date: 2024-09-04'}
+
+
+def test_get_attendance_for_student_in_subject(test_attendance):
+    response = client.get('teacher/attendance/subject/1/student/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [{'id': 1,'class_date': '2024-09-04','status': 'present','added_by': 'Anna Kowalska'}]
