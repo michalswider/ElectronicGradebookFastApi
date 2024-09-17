f1b80d9b007cd36ba0ff1203ca6a40a3fc2efce9
Michał Świderski <michalswiderski41@gmail.com>
2024-09-17 13:49:06 +0200

    Added test_show_profile_detail_without_class in pytest

---
+++ ElectronicGradebook/test/test_student_panel.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+from fastapi import Depends
+from starlette import status
+from starlette.testclient import TestClient
+from ..main import app
+from .utils import *
+from ..routers.auth import get_db, get_current_user
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+client = TestClient(app)
+
+
+def test_show_profile_detail_without_class(test_student):
+    response = client.get('student/profile')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [
+        {'first_name': 'Johny', 'last_name': 'Bravo', 'username': 'j_bravo', 'date_of_birth': '2024-08-18',
+         'class': 'No class assigned', 'role': 'student'}]
