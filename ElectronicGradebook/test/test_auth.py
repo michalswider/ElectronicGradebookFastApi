7163896d7ad4b0578efed1dd64db0ffc384a357d
Michał Świderski <michalswiderski41@gmail.com>
2024-09-24 10:55:00 +0200

    Added test_authenticate_user_wrong_username in pytest

--- ElectronicGradebook/test/test_auth.py
+++ ElectronicGradebook/test/test_auth.py
@@ -13,3 +13,8 @@ def test_authenticate_user(test_student):
     assert authenticated_user is not False
     assert authenticated_user.username == test_student.username
 
+
+def test_authenticate_user_wrong_username():
+    db = TestingSessionLocal()
+    authenticated_user = authenticate_user("wrong_username", 'test1234', db)
+    assert authenticated_user is False
