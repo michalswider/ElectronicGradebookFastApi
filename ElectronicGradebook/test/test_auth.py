ce0682397fd86146e4db943565f801b5f00302ad
Michał Świderski <michalswiderski41@gmail.com>
2024-09-24 11:17:06 +0200

    Added test_get_current_user_valid_token in pytest

--- ElectronicGradebook/test/test_auth.py
+++ ElectronicGradebook/test/test_auth.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from ..main import app
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 from jose import jwt
 
 app.dependency_overrides[get_db] = override_get_db
@@ -37,3 +37,11 @@ def test_create_access_token():
     assert decoded_token['sub'] == username
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    user = await get_current_user(token=token)
+    assert user == {'username': 'testuser', 'id': 1, 'role': 'admin'}
