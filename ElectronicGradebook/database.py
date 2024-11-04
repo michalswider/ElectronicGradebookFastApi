13c5c174c320bc6a6e5883896f5ecbb7ae010cb5
Michał Świderski <michalswiderski41@gmail.com>
2024-11-04 23:27:13 +0100

    Added dockerfile and compose.yml

--- ElectronicGradebook/database.py
+++ ElectronicGradebook/database.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-POSTGRESQL_DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRESQL_DATABASE_URL = os.getenv("POSTGRESQL_DATABASE_URL")
 
 engine = create_engine(POSTGRESQL_DATABASE_URL)
 Base = declarative_base()
