908da126248a7fad8b37f4dee223a4a8f4ad20c8
Michał Świderski <michalswiderski41@gmail.com>
2024-11-07 21:33:06 +0100

    Added automatic admin user creation on app startup

--- ElectronicGradebook/main.py
+++ ElectronicGradebook/main.py
@@ -1,16 +1,28 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request,Depends
 import logging
 from starlette import status
 from starlette.responses import JSONResponse
-from .models import Base
+from .models import Base, User
 from .database import engine
 from .exception import InvalidException, ApplicationException
 from .logging_config import setup_logging
 from .routers import admin_users, admin_subjects, admin_classes, teacher_grades, teacher_attendance, auth, student_panel
+from .routers.auth import get_db, bcrypt_context
+from sqlalchemy.orm import Session
 
 setup_logging()
 
 Base.metadata.create_all(bind=engine)
+
+db_dependency = Annotated[Session,Depends(get_db)]
+
+def create_admin_user(db: db_dependency):
+    admin_user = db.query(User).filter(User.username == "admin").first()
+    if not admin_user:
+        admin_model = User(first_name="admin",last_name="admin",username="admin",hashed_password=bcrypt_context.hash("admin"),role="admin")
+        db.add(admin_model)
+        db.commit()
 
 app = FastAPI()
 
@@ -23,9 +35,13 @@ app.include_router(student_panel.router)
 app.include_router(auth.router)
 
 
-@app.get("/healthy", status_code=status.HTTP_200_OK)
-async def check_status():
-    return {'details': 'Healthy'}
+@app.on_event("startup")
+async def startup_event():
+    db = next(get_db())
+    try:
+        create_admin_user(db)
+    finally:
+        db.close()
 
 
 @app.exception_handler(ApplicationException)
