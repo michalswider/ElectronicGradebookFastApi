769219afaba365c90385919d8abebd6dcad6b704
Michał Świderski <michalswiderski41@gmail.com>
2024-11-22 18:50:58 +0100

    Added validate_username_exist in validation_service.py

--- ElectronicGradebook/services/validation_service.py
+++ ElectronicGradebook/services/validation_service.py
@@ -4,6 +4,8 @@ from fastapi import Depends
 from ..routers.auth import get_db
 from fastapi import HTTPException
 from starlette import status
+from ..models import User
+from ..exception import UsernameAlreadyExistException
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
@@ -12,3 +14,8 @@ def verify_admin_user(user: dict):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authorization failed')
     if user.get('role') != 'admin':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Permission Denied')
+
+def validate_username_exist(user: dict, username: str,db: db_dependency):
+    username_model = db.query(User).filter(User.username == username).first()
+    if username_model is not None:
+        raise UsernameAlreadyExistException(username=username, user=user.get('username'))
