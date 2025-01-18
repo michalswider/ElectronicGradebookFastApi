7a6379ae99a15e97bc60b39528cbdc60e13c0da7
Michał Świderski <michalswiderski41@gmail.com>
2025-01-18 19:35:52 +0100

    Enhance exception handling with additional FastAPI handlers

--- ElectronicGradebook/handlers.py
+++ ElectronicGradebook/handlers.py
@@ -1,6 +1,7 @@
 import logging
 
-from fastapi import Request
+from fastapi import HTTPException, Request
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 from .exception import ApplicationException
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 def setup_exception_handlers(app):
     @app.exception_handler(ApplicationException)
-    async def handle_application_exception(
+    async def custom_exception_handler(
         request: Request, error: ApplicationException
     ) -> JSONResponse:
         logger.error(
@@ -22,3 +23,25 @@ def setup_exception_handlers(app):
             content=content,
             status_code=status_code,
         )
+
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(
+        request: Request, error: HTTPException
+    ) -> JSONResponse:
+        try:
+            user = request.state.user["username"]
+        except (AttributeError, TypeError):
+            user = "Anonymous"
+        logger.error(
+            f"HTTPException caught at {request.url} with method {request.method}, user : {user}. {error}"
+        )
+        content = {"detail": error.detail}
+        return JSONResponse(content=content, status_code=error.status_code)
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(
+        request: Request, error: RequestValidationError
+    ) -> JSONResponse:
+        logger.error(f"Error handler caught pydantic error: {error}")
+        errors = [{"field": e["loc"][-1], "message": e["msg"]} for e in error.errors()]
+        return JSONResponse(content={"detail": errors}, status_code=422)
